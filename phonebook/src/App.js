4c76f2fea8827c7abc485517c08de5c469630a82
Pellinen Juho <juho.pellinen@aalto.fi>
2021-12-26 13:35:07 +0200

    Finished exercise 2.9

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
 
-const Numbers = ({persons}) => {
+const Numbers = (props) => {
   
   return (
     <div>
-      {persons.map(person => 
-        <div key={person.name}>
-          {person.name} {person.number}
-        </div>  
+      {props.persons.map(person =>
+        {if (person.name.includes(props.parameter)) {
+          return <div key={person.name}>{person.name} {person.number}</div>
+        }}
       )}
     </div>
   )
@@ -15,10 +15,14 @@ const Numbers = ({persons}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244' }
+    { name: 'Arto Hellas', number: '040-1231244' },
+    { name: 'testi', number: '0502200'},
+    { name: 'aa', number: '0400400400'},
+    { name: 'fgta', numer: '04512345678'}
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newParameter, setNewParameter] = useState('')
   
   const addName = (event) => {
     event.preventDefault()
@@ -36,25 +40,34 @@ const App = () => {
   }
 
   const nameChangeHandler = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const numberChangeHandler = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewNumber(event.target.value)
+  }
+
+  const filterChangeHandler = (event) => {
+    //console.log(event.target.value)
+    setNewParameter(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form >
+        <div>Filter: <input value={newParameter} onChange={filterChangeHandler}/></div>
+      </form>
+      <h2>Add a new person</h2>
       <form onSubmit={addName}>
         <div>name: <input value={newName} onChange={nameChangeHandler}/></div>
         <div>number: <input value={newNumber} onChange={numberChangeHandler}/></div>
         <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-      <Numbers persons={persons} />
+      <Numbers persons={persons} parameter={newParameter}/>
     </div>
   )
 
